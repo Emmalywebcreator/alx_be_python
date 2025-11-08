89c7ccb92cc83db838fe4ceb6e6cca2ae2a8aeb4
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-08 23:02:30 +0100

    This file performs three mathematical operation - Addition, Subtraction and Multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+number1 = 10
+number2 = 5
+
+"""Addition of numbers"""
+sum = number1 + number2
+
+"""Subtraction"""
+diff = number1 - number2
+
+"""Multiplication"""
+mul = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {mul}")
