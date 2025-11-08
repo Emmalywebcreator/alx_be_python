8c45825eeb3b0ff8c94ad45525e0af0a4aff33a5
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-11-09 00:29:45 +0100

    Tis script calculated the monthly savings and the projected savings at a particular period of time

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+monthly_income = int(input("Enter your monthly income: "))
+total_expenses = int(input("Enter your total monthly income: "))
+
+monthly_savings = monthly_income - total_expenses
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
